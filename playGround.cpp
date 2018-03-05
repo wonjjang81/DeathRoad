8079a985ce6464038d3a70768234331c97a2171a
wonjjang81 <35555316+wonjjang81@users.noreply.github.com>
2018-03-05 20:31:17 +0900

    frameWork_api

---
+++ playGround.cpp
@@ -0,0 +1,52 @@
+#include "stdafx.h"
+#include "playGround.h"
+
+
+playGround::playGround()
+{
+}
+
+
+playGround::~playGround()
+{
+
+}
+
+HRESULT playGround::init()
+{
+	gameNode::init(true);
+
+
+	
+	
+	return S_OK;
+}
+
+//메모리 관련 삭제
+void playGround::release(void)
+{
+	gameNode::release();
+
+}
+
+//연산
+void playGround::update(void)
+{
+	gameNode::update();
+
+	//SCENEMANAGER->update();
+}
+
+//그리는거.......
+void playGround::render(void)
+{
+	PatBlt(getMemDC(), 0, 0, WINSIZEX, WINSIZEY, WHITENESS);
+	//================== 이 위는 손대지 마시오 =========================
+
+	//SCENEMANAGER->render();
+
+	//================== 이 아래는 손대지 마시오 ========================
+	this->getBackBuffer()->render(getHDC(), 0, 0);//hdc영역에 그려준다 
+}
+
+
